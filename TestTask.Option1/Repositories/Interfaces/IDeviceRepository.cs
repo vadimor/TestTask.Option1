aaaf4fbd90ece4d32843896014d35cb9ed61b2f5
VadymBilyi <vadimorbiluy@gmail.com>
2023-02-21 12:37:45 +0200

    Add repository interfaces

---
+++ TestTask.Option1/Repositories/Interfaces/IDeviceRepository.cs
@@ -0,0 +1,15 @@
+﻿using TestTask.Option1.Data.Entities;
+
+namespace TestTask.Option1.Repositories.Interfaces
+{
+    // This interface provides methods for working with the Device table
+    public interface IDeviceRepository
+    {
+
+        // This method adds a device to the table
+        public Task<Device> AddDeviceAsync(string deviceToken);
+
+        // This method retrieves a device by its device token
+        public Task<Device?> GetDeviceAsync(string deviceToken);
+    }
+}
