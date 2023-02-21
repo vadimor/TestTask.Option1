6cb1d6d61aa37f66e2e7b0a27f0525659aa98348
VadymBilyi <vadimorbiluy@gmail.com>
2023-02-21 12:48:14 +0200

    Add an interface for working with experiment system

---
+++ TestTask.Option1/Services/Interfaces/IExperimentManageService.cs
@@ -0,0 +1,17 @@
+﻿using TestTask.Option1.Models.Dtos;
+using TestTask.Option1.Models.Response;
+
+namespace TestTask.Option1.Services.Interfaces
+{
+    // This interface provides methods for working with the experiment system
+
+    public interface IExperimentManageService
+    {
+        // This method return one of several options for experiment values
+        // One device token should always have the same experiment value for every request 
+        public Task<ExperimentValueDto?> GetExperimentValueAsync(string experimentName, string deviceToken);
+
+        // This method returns statistics about the results of experiments
+        public Task<IReadOnlyCollection<ExperimentStatisticResponse>> GetStaticticsAsync();
+    }
+}
