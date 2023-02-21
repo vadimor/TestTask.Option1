9583b3467cba8ce81363c462a4312560f263f652
VadymBilyi <vadimorbiluy@gmail.com>
2023-02-21 13:01:12 +0200

    Add HttpGlobalException to chatching exceptions

---
+++ TestTask.Option1/Filters/HttpGlobalExceptionFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace TestTask.Option1.Filters
+{
+    // This class catches all exceptions that were not caught.
+
+    public class HttpGlobalExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<HttpGlobalExceptionFilter> _logger;
+
+        public HttpGlobalExceptionFilter(ILogger<HttpGlobalExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(
+                new EventId(context.Exception.HResult),
+                context.Exception,
+                context.Exception.Message);
+        }
+    }
+}
