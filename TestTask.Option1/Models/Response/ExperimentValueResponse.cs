6cfba4e8336f968279d909e2ab4dd93200eea393
VadymBilyi <vadimorbiluy@gmail.com>
2023-02-21 12:58:30 +0200

    Add ExperimentValueResponse

---
+++ TestTask.Option1/Models/Response/ExperimentValueResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TestTask.Option1.Models.Response
+{
+    public class ExperimentValueResponse
+    {
+        public string Key { get; set; } = null!;
+
+        public string Value { get; set; } = string.Empty;
+    }
+}
