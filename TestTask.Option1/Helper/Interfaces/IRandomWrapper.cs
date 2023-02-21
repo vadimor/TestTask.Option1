b2619ead1ce3e68556b5f6045b71385e911834fe
VadymBilyi <vadimorbiluy@gmail.com>
2023-02-21 12:44:55 +0200

    Add a wrapper to working with randomizing numbers

---
+++ TestTask.Option1/Helper/Interfaces/IRandomWrapper.cs
@@ -0,0 +1,9 @@
+﻿namespace TestTask.Option1.Helper.Interfaces
+{
+    // This interface provides a wrapper for working with random numbers 
+
+    public interface IRandomWrapper
+    {
+        public float NextSingle();
+    }
+}
---
+++ TestTask.Option1/Helper/RandomWrapper.cs
@@ -0,0 +1,21 @@
+﻿using TestTask.Option1.Helper.Interfaces;
+
+namespace TestTask.Option1.Helper
+{
+    // This class provides a wrapper for working with random numbers 
+
+    public class RandomWrapper : IRandomWrapper
+    {
+        private readonly Random _random;
+
+        public RandomWrapper()
+        {
+            _random = new Random();
+        }
+
+        public float NextSingle()
+        {
+            return _random.NextSingle();
+        }
+    }
+}
