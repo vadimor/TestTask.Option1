31bcff558d67b8dda3ae7b902b753a5cd7abdb01
VadymBilyi <vadimorbiluy@gmail.com>
2023-02-21 12:53:39 +0200

    Add ExperimentValueResponse

---
+++ TestTask.Option1/Models/Response/ExperimentStatisticResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace TestTask.Option1.Models.Response
+{
+    public class ExperimentStatisticResponse
+    {
+        public string ExperimentName { get; set; } = null!;
+
+        public IDictionary<string, int> ValueStatistic { get; set; } = new Dictionary<string, int>();
+    }
+}
