6abd8128dd9a61976bc4c412529891dc8663696b
VadymBilyi <vadimorbiluy@gmail.com>
2023-02-21 12:42:56 +0200

    Add an abstract class to work with the database

---
+++ TestTask.Option1/Data/BaseDataService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Option1.Data.Interfaces;
+
+namespace TestTask.Option1.Data
+{
+    // This class provides methods for wrapping code that changes the database
+
+    public abstract class BaseDataService<T>
+        where T : DbContext
+    {
+        private readonly ILogger<BaseDataService<T>> _logger;
+        private readonly IDbContextWrapper<T> _dbContextWrapper;
+
+        public BaseDataService(ILogger<BaseDataService<T>> logger, IDbContextWrapper<T> dbContextWrapper)
+        {
+            _logger = logger;
+            _dbContextWrapper = dbContextWrapper;
+        }
+
+        protected async Task ExecuteSafeAsync(Func<Task> action)
+        {
+            await using var transaction = await _dbContextWrapper.BeginTransactionAsync();
+
+            try
+            {
+                await action();
+
+                await transaction.CommitAsync();
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                _logger.LogError(ex, "transaction is rollbacked");
+            }
+        }
+
+        protected async Task<TResult> ExecuteSafeAsync<TResult>(Func<Task<TResult>> action)
+        {
+            await using var transaction = await _dbContextWrapper.BeginTransactionAsync();
+
+            try
+            {
+                var result = await action();
+
+                await transaction.CommitAsync();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                _logger.LogError(ex, $"transaction is rollbacked");
+            }
+
+            return default!;
+        }
+    }
+}
---
+++ TestTask.Option1/Data/DbContextWrapper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using TestTask.Option1.Data.Interfaces;
+
+namespace TestTask.Option1.Data
+{
+    // This class provides a wrapper for working with transactions 
+
+    public class DbContextWrapper<T> : IDbContextWrapper<T>
+        where T : DbContext
+    {
+        private readonly T _dbContext;
+
+        public DbContextWrapper(IDbContextFactory<T> dbContextFactory)
+        {
+            _dbContext = dbContextFactory.CreateDbContext();
+        }
+
+        public T DbContext => _dbContext;
+
+        public async Task<IDbContextTransaction> BeginTransactionAsync()
+        {
+            return await _dbContext.Database.BeginTransactionAsync();
+        }
+    }
+}
